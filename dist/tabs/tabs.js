c0344c767ea209da685c437b5c339c0171098f53
James Fleeting <hello@jamesfleeting.com>
2017-01-26 09:54:07 -0600

    🎨  Minor cleanup in the tabs JavaScript.

--- dist/tabs/tabs.js
+++ dist/tabs/tabs.js
@@ -15,42 +15,6 @@
 
       var tabList = tab.querySelectorAll('li.tab');
 
-      // Change active tab on click
-      var makeActive = function makeActive() {
-        var _iteratorNormalCompletion2 = true;
-        var _didIteratorError2 = false;
-        var _iteratorError2 = undefined;
-
-        try {
-          for (var _iterator2 = tabList[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-            var tabItem = _step2.value;
-
-            tabItem.classList.remove('active');
-            tabItem.children[0].classList.remove('active');
-            tabItem.children[1].classList.remove('is-current');
-          }
-        } catch (err) {
-          _didIteratorError2 = true;
-          _iteratorError2 = err;
-        } finally {
-          try {
-            if (!_iteratorNormalCompletion2 && _iterator2.return) {
-              _iterator2.return();
-            }
-          } finally {
-            if (_didIteratorError2) {
-              throw _iteratorError2;
-            }
-          }
-        }
-
-        ;
-
-        this.classList.add('active');
-        this.children[0].classList.add('active');
-        this.children[1].classList.add('is-current');
-      };
-
       var _iteratorNormalCompletion3 = true;
       var _didIteratorError3 = false;
       var _iteratorError3 = undefined;
@@ -59,7 +23,9 @@
         for (var _iterator3 = tabList[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
           var tabItem = _step3.value;
 
-          tabItem.addEventListener('click', makeActive);
+          tabItem.addEventListener('click', function () {
+            tabMakeActive(this, tabList);
+          });
         }
       } catch (err) {
         _didIteratorError3 = true;
@@ -93,5 +59,38 @@
         throw _iteratorError;
       }
     }
+  }
+
+  function tabMakeActive(element, tabList) {
+    var _iteratorNormalCompletion2 = true;
+    var _didIteratorError2 = false;
+    var _iteratorError2 = undefined;
+
+    try {
+      for (var _iterator2 = tabList[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+        var tabItem = _step2.value;
+
+        tabItem.classList.remove('active');
+        tabItem.children[0].classList.remove('active');
+        tabItem.children[1].classList.remove('is-current');
+      }
+    } catch (err) {
+      _didIteratorError2 = true;
+      _iteratorError2 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion2 && _iterator2.return) {
+          _iterator2.return();
+        }
+      } finally {
+        if (_didIteratorError2) {
+          throw _iteratorError2;
+        }
+      }
+    }
+
+    element.classList.add('active');
+    element.children[0].classList.add('active');
+    element.children[1].classList.add('is-current');
   }
 })();
--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -6,21 +6,20 @@
   for (const tab of tabs) {
     let tabList = tab.querySelectorAll('li.tab');
 
-    // Change active tab on click
-    const makeActive = function() {
-      for (const tabItem of tabList) {
-        tabItem.classList.remove('active');
-        tabItem.children[0].classList.remove('active');
-        tabItem.children[1].classList.remove('is-current');
-      };
-
-      this.classList.add('active');
-      this.children[0].classList.add('active');
-      this.children[1].classList.add('is-current');
-    };
-
     for (const tabItem of tabList) {
-      tabItem.addEventListener('click', makeActive);
+      tabItem.addEventListener('click', function() { tabMakeActive(this, tabList); });
     }
+  }
+
+  function tabMakeActive(element, tabList) {
+    for (const tabItem of tabList) {
+      tabItem.classList.remove('active');
+      tabItem.children[0].classList.remove('active');
+      tabItem.children[1].classList.remove('is-current');
+    }
+
+    element.classList.add('active');
+    element.children[0].classList.add('active');
+    element.children[1].classList.add('is-current');
   }
 }());
